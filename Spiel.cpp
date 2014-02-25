388a085f426509ec7764a183ef5ed3ff267455b6
l2iisk <l2iisk@web.de>
2014-02-25 06:35:32 -0500

    Spiel.cpp
    bla

---
+++ Spiel.cpp
@@ -0,0 +1,170 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+
+int z;
+int x = 0;
+int y=20;
+int a,b,c,d,e,f,g,h,i;
+
+cout << "Wir brauchen 6 Zahlen: " << endl;
+
+while(y!=89494616846846)
+{
+	cout << "Geben Sie eine ganze Zahl ein die groesser als 2 ist und kleiner als 100 ist: ";
+	cin >> y;
+	if(y>2&&y<100){cout << endl << "Dankeschoen!"; break;}
+	else{cout << endl << "Vollidiot! Nochmal!";}
+}
+
+if(y==42&&i!=1){cout << "Achievement unlocked! Du bist ja ganz schlau!" << endl; i=1;}
+
+while(a!=89494616846846)
+{
+	cout << "Geben Sie eine ganze Zahl ein die groesser als 45 ist und kleiner als 99 ist: ";
+	cin >> a;
+	if(a>45&&a<99){cout << endl << "Dankeschoen!"; break;}
+	else{cout << endl << "Vollidiot! Nochmal!";}
+}
+
+if(y==42&&i!=1){cout << "Achievement unlocked! Du bist ja ganz schlau!" << endl; i=1;}
+
+while(b!=89494616846846)
+{
+	cout << "Geben Sie eine ganze Zahl ein die groesser als 0 ist und kleiner als 17 ist: ";
+	cin >> b;
+	if(b>0&&b<17){cout << endl << "Dankeschoen!"; break;}
+	else{cout << endl << "Vollidiot! Nochmal!";}
+}
+
+if(y==42&&i!=1){cout << "Achievement unlocked! Du bist ja ganz schlau!" << endl; i=1;}
+
+while(c!=89494616846846)
+{
+	cout << "Geben Sie eine ganze Zahl ein die groesser als 5 ist und kleiner als 43 ist: ";
+	cin >> c;
+	if(c>5&&c<43){cout << endl << "Dankeschoen!"; break;}
+	else{cout << endl << "Vollidiot! Nochmal!";}
+}
+
+if(y==42&&i!=1){cout << "Achievement unlocked! Du bist ja ganz schlau!" << endl; i=1;}
+
+while(d!=89494616846846)
+{
+	cout << "Geben Sie eine ganze Zahl ein die groesser als 34 ist und kleiner als 68 ist: ";
+	cin >> d;
+	if(d>34&&d<68){cout << endl << "Dankeschoen!"; break;}
+	else{cout << endl << "Vollidiot! Nochmal!";}
+}
+
+if(y==42&&i!=1){cout << "Achievement unlocked! Du bist ja ganz schlau!" << endl; i=1;}
+
+while(e!=89494616846846)
+{
+	cout << "Geben Sie eine ganze Zahl ein die groesser als 12 ist und kleiner als 55 ist: ";
+	cin >> e;
+	if(e>12&&e<55){cout << endl << "Dankeschoen!"; break;}
+	else{cout << endl << "Vollidiot! Nochmal!";}
+}
+
+if(y==42&&i!=1){cout << "Achievement unlocked! Du bist ja ganz schlau!" << endl; i=1;}
+
+// while(f!=89494616846846)
+// {
+// 	cout << "Geben Sie eine ganze Zahl ein die groesser als 42 ist und kleiner als 69 ist: ";
+// 	cin >> f;
+// 	if(f>42&&f<69){cout << endl << "Dankeschoen!"; break;}
+// 	else{cout << endl << "Vollidiot! Nochmal!";}
+// }
+
+// if(y==42&&i!=1){cout << "Achievement unlocked! Du bist ja ganz schlau!" << endl; i=1;}
+
+// while(g!=89494616846846)
+// {
+// 	cout << "Geben Sie eine ganze Zahl ein die groesser als 7 ist und kleiner als 9 ist: ";
+// 	cin >> g;
+// 	if(g>7&&g<9){cout << endl << "Dankeschoen!"; break;}
+// 	else{cout << endl << "Vollidiot! Nochmal!";}
+// }
+
+// if(y==42&&i!=1){cout << "Achievement unlocked! Du bist ja ganz schlau!" << endl; i=1;}
+
+// while(h!=89494616846846)
+// {
+// 	cout << "Geben Sie eine ganze Zahl ein die groesser als 8 ist und kleiner als 100 ist: ";
+// 	cin >> h;
+// 	if(h>8&&h<100){cout << endl << "Dankeschoen!"; break;}
+// 	else{cout << endl << "Vollidiot! Nochmal!";}
+// }
+
+// if(y==42&&i!=1){cout << "Achievement unlocked! Du bist ja ganz schlau!" << endl; i=1;}
+
+system("CLS");
+
+	int j;
+	cout << "Auf einer Skala von 1-10, wie cool bist du? ";
+	cin >> j;
+
+	string k;
+
+	cout << endl << "Gebe bitte deinen Namen ein: ";
+	cin >> k;
+
+	if(k=="Momo"){cout << endl << "Du heisst Maurice du Idiot!!" << endl << endl;}
+	if(k=="Maurice"){cout << endl << "Du heisst Momo du Idiot!!" << endl << endl;}
+	if(k!="Momo"&&k!="Dennis"&&k!="Alex"&&k!="Erik"&&k!="Marius"&&k!="Max"&&k!="Wilko"&&k!="Alex"&&k!="Robert"&&k!="Umberto"&&k!="William"&&k!="Maurice"&&k!="Sven"&&k!="Marc"&&k!="David"&&k!="Micha"){cout << endl << "Wer bist denn du??? Mit dir habe ich ja nicht gerechnet! Willkommen bei meinem Spiel " << k << "! Ich hoffe du hast Spass!" << endl << endl;}
+	if(k=="Sven"){cout << endl << "Du heisst Swagger-Sven du Idiot!!" << endl << endl;}
+	if(k=="Marc"){cout << endl << "Du Idiot!!" << endl << endl;}
+	if(k=="David"){cout << endl << "Du Idiot!! Ich bin besser in Ex als du!!" << endl << endl;}
+	if(k=="Micha"){cout << endl << "Du heisst Russe du Idiot!!" << endl << endl;}
+	if(k=="William"){cout << endl << "Ey William, was geht?!" << endl << endl;}
+	if(k=="Umberto"){cout << endl << "Sie rufen an um was?????!!!!" << endl << endl;}
+	if(k=="Robert"){cout << endl << "Du heisst Umberto du Idiot!!" << endl << endl;}
+	if(k=="Alex"){cout << endl << "Was geht du Lauch?" << endl << endl;}
+	if(k=="Dennis"){cout << endl << "Scheiss auf Belgien!" << endl << endl;}
+	if(k=="Erik"){cout << endl << "Stolper nicht!" << endl << endl;}
+	if(k=="Marius"){cout << endl << "Hola Marius!" << endl << endl;}
+	if(k=="Max"){cout << endl << "Heirate eine Mustermann!!!!" << endl << endl;}
+	if(k=="Wilko"){cout << endl << "Zock nicht so viel!" << endl << endl;}
+
+
+switch (j){
+
+
+	case 10: {cout << "Cooler Typ!"; break;}
+	case 9: {cout << "Cooler Typ!"; break;}
+	case 8: {cout << "Cooler Typ!"; break;}
+	default: {cout << "Du bist eindeutig cooler!"; break;}
+	case 2: {cout << "Du bist ja komisch!"; break;}
+	case 1: {cout << "Du bist ja komisch!"; break;}
+
+}
+
+
+
+
+
+while(z!=y)
+{
+	cout << "Rate eine Zahl zwischen 0 und 101 : ";
+	cin >> z;
+	x+=1;
+	if(z!=c){cout << endl << "Schade versuch es nochmal!" << endl;}
+	if(z==c&&x<=3){cout << endl << "Du bist ja toll! Du konntest dir eine Zahl merken! Du hast nur " << x << " Versuche gebraucht!";}
+	if(z==c&&x>3&&x<=5){cout << endl << "Das war ja nicht so gut! Du hast ganze " << x << " Versuche gebraucht!";}
+	if(z==c&&x>5){cout << endl << "Wie dumm du bist!! Du hast " << x << " Versuche gebraucht!!!! Und du musstest dir nur eine Zahl merken!!";}
+	if(z<c){cout << "Die Zahl die du raten sollst ist groesser!";}
+	if(z>c){cout << "Die Zahl die du raten sollst ist kleiner!";}
+	if(x>10){cout << endl << "Wie dumm du bist!! Du musstest dir nur eine Zahl merken und hast mehr als 10 Versuche gebraucht!!"; break;}
+}
+
+
+
+
+	return 0;
+}
